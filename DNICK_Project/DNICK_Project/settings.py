104280c4fc195c7ef68250da6bc3f53bc5906891
stoilovska <86618434+stoilovska@users.noreply.github.com>
2023-06-26 17:44:53 +0200

    Update settings.py

--- DNICK_Project/DNICK_Project/settings.py
+++ DNICK_Project/DNICK_Project/settings.py
@@ -79,9 +79,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'OPTIONS': {
-            'version': '3.8.3',
-        },
     }
 }
 
